4e4f5d9f3a39da3cd9881a1b247ee123bc7d926e
KRISH CHAUDHARY <162810813+krish1440@users.noreply.github.com>
2025-12-06 23:23:55 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-/// <reference types="node" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 });
